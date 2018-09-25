73195e3a6dabeb9c4ad196988ec4134fc3fe67de
Elizabeth Fancher <42560254+ElizabethFancher@users.noreply.github.com>
2018-09-25 19:52:08 -0400

    Building dance class website
    Stopping for now

---
+++ Form Assignment/efancher/FormPlay.js
@@ -0,0 +1,6 @@
+function tyAlert () {
+    alert("Thank you for signing up for one of our dance classes. \n\nYour instructor will contact you soon!");
+}
+function classes() {
+    window.open("Classes.html", "_blank", "width=610, height=360");
+}
