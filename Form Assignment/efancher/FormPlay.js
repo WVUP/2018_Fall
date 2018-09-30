8e763e110ecc38eeaeb466b984096ad2232953bf
Elizabeth Fancher <42560254+ElizabethFancher@users.noreply.github.com>
2018-09-29 22:17:43 -0400

    Form assignment with alert output

--- Form Assignment/efancher/FormPlay.js
+++ Form Assignment/efancher/FormPlay.js
@@ -5,14 +5,15 @@
 function classes() {
     window.open("Classes.html", "_blank", "width=610, height=360");
 }
-function mondayBallet3()
-{
-    window.open("mondayBallet3.html", "_blank", "width=610, height=360");
-}
-function mondayBallet7()
-{
-    window.open("Classes.html", "_blank", "width=610, height=360");
-}
+// function mondayBallet3()
+// {
+//     window.open("mondayBallet3.html", "_blank", "width=610, height=360");
+//     getMondayBallet3(personList);
+// }
+// function mondayBallet7()
+// {
+//     window.open("Classes.html", "_blank", "width=610, height=360");
+// }
 
 let form = document.getElementById('submitForm');
 console.dir(form);
@@ -20,7 +21,17 @@ console.dir(form);
 let submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', formSubmit);
 
+// let populateList = document.getElementById('classList');
+// populateList.addEventListener('click', addStudent);
+
 let personList = [];
+var person;
+let student;
+var successName;
+var successDate;
+var successContactVia;
+var successDance;
+var successTime;
 
 function formSubmit(event)
 {
@@ -34,8 +45,9 @@ function formSubmit(event)
     let message = getMessage();
     let phone = getPhoneNumber();
     let day = getDayOfWeek();
-    let person = {
-        dance,
+    let username = getUserID();
+    person = [
+        {dance,
         name,
         time,
         email,
@@ -43,20 +55,53 @@ function formSubmit(event)
         contactVia,
         message,
         phone,
-        day
-    };
+        day,
+        username}
+    ]
 
+    var item = {person, done: false};
+    window.localStorage.setItem('student', JSON.stringify(person));
     personList.push(person);
+    printSuccess(name, date, dance, contactVia, time)
     console.log(person);
 }
-function findMondayBalletClass3()
+function printSuccess(name, date, dance, contactVia, time)
 {
-    personList.find(item =>
-        {
-            if (item.dance == "ballet"); AND (item.date == "Monday"); AND (item.time == "3:00 PM");{
-                return item};
-        });
+    alert('Congratulations '+ name +'! You have successfully signed up for the '+ dance +' class on '+ date +' at '+ time +'! Your instructor will contact you soon by '+ contactVia +'. Have a good day!');
 }
+// function addStudent (e)
+// {
+//     e.preventDefault();
+//     const text = (this.querySelector('[name=danceForm]')).value;
+//     const item = {
+//         text,
+//         done: false
+//     };
+//     items.push(item);
+//     this.reset();
+// }
+
+// function getMondayBallet3(personList) {
+    
+//     let html = '';
+
+//     for (let i = 0; i < personList.length; i++) {
+//         if (personList[i].dance == "ballet") {
+//             if (personList[i].day == "Monday"){
+//                 if(personList[i].time == "3:00") {
+//                     return 
+//                     `<li>
+//                         ${personList[i].username}
+//                     </li>`
+//                 }
+//             }
+//         }
+//         // html += `<div>${personList[i].username} input: ${monBallet3.classList2.value}</div>`
+//     }
+//     console.log(html);
+//     document.getElementById('mondayBallet3Class').innerHTML = html;
+//     join('');
+// }
 
 function getName(fname, lname)
 {
@@ -69,6 +114,20 @@ function getName(fname, lname)
     let nameInput = firstNameEntered + ' ' + lastNameEntered;
 
     return nameInput;
+}
+function getUserID(fname, lname)
+{
+    var firstNameEntered;
+    var lastNameEntered;
+    var firstInitial;
+    
+    firstNameEntered = document.getElementById("firstName").value;
+    firstInitial = firstNameEntered.charAt(0);
+    lastNameEntered = document.getElementById("lastName").value;
+
+    let username = firstInitial + lastNameEntered;
+
+    return username;
 }
 function getEmail()
 {
@@ -155,7 +214,7 @@ function getDayOfWeek()
     let dayInputs;
 
     days = form.querySelector('[data-day]');
-    dayInputs = dances.querySelectorAll('input');
+    dayInputs = days.querySelectorAll('input');
 
     dayInputs.forEach(item => {
         if(item.checked == true){
