daee02ab6346a371d3ef0acfdfe849ec5e6f5d03
dorr1 <dorr1@wvup.edu>
2018-11-30 22:26:23 -0500

    password validation

--- final/markup/dorr1/Booster.js
+++ final/markup/dorr1/Booster.js
@@ -1,17 +1,70 @@
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var conf = document.getElementById("confpassword")
+var same = document.getElementById("same")
+
+myInput.onkeyup = function () {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if (myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+}
+
+// Validate Password Confirmation
+conf.onkeyup = function () {
+    var passw = $("#password").val();
+    if (conf.value.match(passw)) {
+        same.classList.remove("invalid");
+        same.classList.add("valid");
+    } else {
+        same.classList.remove("valid");
+        same.classList.add("invalid");
+    }
+
+    
+}
+
+
 function Reset() {
     confirm("Are you sure you want to start over!");
 }
 
-function checkPasswordMatch() {
-    var password = $("#password").val();
-    var confirmPassword = $("#confpassword").val();
 
-    if (password != confirmPassword)
-        $("#CheckPasswordMatch").html("Passwords do not match!");
-    else
-        $("#CheckPasswordMatch").html("");
-}
 
-$(document).ready(function () {
-   $("#password, #confpassword").keyup(checkPasswordMatch);
-});
