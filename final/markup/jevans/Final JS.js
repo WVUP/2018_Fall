69e65bd1b4c8211dbee39502443dd0a510d57552
JamesEvans1 <43153193+JamesEvans1@users.noreply.github.com>
2018-12-10 16:12:14 -0500

    Final Project

---
+++ final/markup/jevans/Final JS.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+function getsDateOfBirth(dateOfBirth)
+{
+    var requestStartEntered;
+
+    requestStartEntered = document.getElementById("reqStart").value;
+    return requestStartEntered;
+}
+
+function getsFirstName(FirstName)
+{
+    var firstNameEntered;
+
+    firstNameEntered = document.getElementById("fName").value;
+    return firstNameEntered;
+}
+
+function getsLastName(LastName)
+{
+    var lastNameEntered;
+
+    lastNameEntered = document.getElementById("lName").value;
+    return lastNameEntered;
+}
